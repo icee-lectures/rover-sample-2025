4ef9bd5fe756e931a63dfff0bd8a3f4b25c915c6
kageyama_trdi <tkageyama@tottori-u.ac.jp>
2025-12-19 12:21:39 +0900

    OrbbecSDK ROS2 を使うように変更

--- rover/rover_ws/src/rover/launch/rover_launch.py
+++ rover/rover_ws/src/rover/launch/rover_launch.py
@@ -1,24 +1,35 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     return LaunchDescription([
-        # camera - camera node
-        Node(
-            package='camera',
-            executable='camera',
-            name='camera',
-            output='screen',
+        # orbbec_camera の astra_pro_plus を起動
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('orbbec_camera'),
+                    'launch',
+                    'astra_pro_plus.launch.py',
+                )
+            )
         ),
-        # camera - aruco_detector node
+        # camera パッケージの aruco_detector ノード
         Node(
             package='camera',
             executable='aruco_detector',
             name='aruco_detector',
             output='screen',
+            parameters=[
+                {'image_topic': 'camera/color/image_raw/compressed'},
+            ],
         ),
-        # robot_control_board - driver_node
+        # robot_control_board の driver_node ノード
         Node(
             package='robot_control_board',
             executable='driver_node',
